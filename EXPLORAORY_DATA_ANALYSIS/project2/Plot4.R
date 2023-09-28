bb9a453ce7d25d6639c2919d34f574934c811bda
Vishnu <106979783+Vishnu746go@users.noreply.github.com>
2023-09-28 21:51:27 +0530

    Update Plot4.R

--- EXPLORAORY_DATA_ANALYSIS/project2/Plot4.R
+++ EXPLORAORY_DATA_ANALYSIS/project2/Plot4.R
@@ -4,8 +4,8 @@ library(dplyr)
 library(ggplot2)
 
 # Read the NEI and SCC data
-NEI_data <- readRDS("C:/Users/vishn/Downloads/mydownloads/EDA IN R/exdata-data-NEI_data/summarySCC_PM25.rds")
-SCC_data <- readRDS("C:/Users/vishn/Downloads/mydownloads/EDA IN R/exdata-data-NEI_data/Source_Classification_Code.rds")
+NEI_data <- readRDS("C:/Users/vishn/Downloads/mydownloads/EDA IN R/exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC_data <- readRDS("C:/Users/vishn/Downloads/mydownloads/EDA IN R/exdata_data_NEI_data/Source_Classification_Code.rds")
 
 # Filter SCC data for combustion-related and coal-related sources
 combustionRelated <- grepl("comb", SCC_data$SCC.Level.One, ignore.case = TRUE)
@@ -15,11 +15,14 @@ combustionSCC <- SCC_data[combustionRelated & coalRelated, "SCC"]
 # Subset NEI data for combustion-related SCCs
 combustionNEI <- NEI_data[NEI_data$SCC %in% combustionSCC, ]
 
-# Create a ggplot2 bar plot and display it
+# Create a ggplot2 bar plot
 ggplot(combustionNEI, aes(x = factor(year), y = Emissions / 10^5)) +
   geom_bar(stat = "identity", fill = "#FF9999", width = 0.75) +
+  
+  # Customize axis and title labels
   labs(
     x = "Year",
     y = expression("Total PM"[2.5]*" Emission (10^5 Tons)"),
-    title = expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008")
+    title = expression("PM"[2.5]*" Coal Combustion Source Emissions Across US (1999-2008)")
   )
+
